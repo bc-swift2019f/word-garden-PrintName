ce673de2a670f5dcae039c80a2b66fb19344dc8f
Allen <54394182+PrintName@users.noreply.github.com>
2019-09-15 21:53:05 -0400

    3.1 Complete

---
+++ WordGarden/WordGarden/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordGarden
+//
+//  Created by Allen Li on 9/15/19.
+//  Copyright © 2019 Allen Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
